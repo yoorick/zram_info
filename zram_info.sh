6f94444813a3e104e9517a6debe97ea39688de33
yoorick <yoory.nagumanov@gmail.com>
2019-02-20 22:45:55 +0600

    Show compression ratio in percentage.

--- zram_info.sh
+++ zram_info.sh
@@ -3,28 +3,44 @@
 sum_compr=0
 sum_orig=0
 
-divisor=$(( 1024 * 1024 ))
+function to_mib() {
+    local size_in_bytes=$1
+    echo "$(( size_in_bytes / (1024 * 1024) ))"
+}
 
 devices=$( ls /sys/block/ | grep zram )
 
+echo 'Device: Compressed Size / Original Size / Max Size   (Compression Ratio)'
+
 for i in ${devices} ; do
-    compr=$( cat /sys/block/${i}/compr_data_size )
-    orig=$( cat /sys/block/${i}/orig_data_size )
-    max=$( cat /sys/block/${i}/disksize )
-    
+
+    compr=$(< /sys/block/${i}/compr_data_size )
+    orig=$(< /sys/block/${i}/orig_data_size )
+    max=$(< /sys/block/${i}/disksize )
+
     (( sum_compr += compr ))
     (( sum_orig += orig ))
-    
-    echo "${i}: $(( compr / divisor )) MiB / $(( orig / divisor )) MiB / $(( max / divisor )) MiB   (${compr} / ${orig})";
+
+    compr_mib=$( to_mib "${compr}" )
+    orig_mib=$( to_mib "${orig}" )
+    max_mib=$( to_mib "${max}" )
+
+    (( orig > 0 )) && ratio=$(( 100 * compr / orig )) || ratio='N/A'
+
+    echo "${i}: ${compr_mib} MiB / ${orig_mib} MiB / ${max_mib} MiB   (${ratio} %)";
+
 done
 
-echo "total: $(( sum_compr / divisor )) MiB / $(( sum_orig / divisor )) MiB"
+sum_compr_mib=$( to_mib "${sum_compr}" )
+sum_orig_mib=$( to_mib "${sum_orig}" )
+
+echo "Total: ${sum_compr_mib} MiB / ${sum_orig_mib} MiB"
 
 swap=( $( free -m | grep "^Swap:" ) )
 mem=( $(free -m | grep "^-/+\sbuff") )
 
-echo "swap: ${swap[2]} MiB"
-echo "mem: ${mem[2]} MiB used + ${mem[3]} MiB free"
+echo "Swap: ${swap[2]} MiB"
+echo "Memory: ${mem[2]} MiB used + ${mem[3]} MiB free"
 
 
 
